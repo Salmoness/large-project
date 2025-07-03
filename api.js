8ece0a3f814e78181a31bc0fb717c00e5d62e967
saymon <slrivas24@gmail.com>
2025-07-02 20:21:39 -0400

    implemented JWT on all APIs

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 require("express");
 require("mongodb");
+const token = require("./createJWT.js");
 
 const MOCK_USERNAME = "user";
 const MOCK_PASSWORD = "password";
@@ -10,14 +11,17 @@ exports.setApp = function(app, client)
     app.post('/api/login', async (req, res, next) =>
     {
         // incoming: login, password
-        // outgoing: id, firstName, lastName, error
-        var error = '';
+        // outgoing: error, jwtToken
+        
         const { login, password } = req.body;
         const db = client.db('COP4331Cards');
         const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
         var id = -1;
         var fn = '';
         var ln = '';
+
+        let error = '';
+        let newToken = null;
         if( results.length > 0 )
         {
             id = results[0].UserID;
@@ -25,12 +29,11 @@ exports.setApp = function(app, client)
             ln = results[0].LastName;
             try
             {
-                const token = require("./createJWT.js");
-                ret = token.createToken( fn, ln, id );
+                newToken = token.createToken( fn, ln, id );
             }
             catch(e)
             {
-                ret = {error:e.message};
+                error = e.message;
             }
         }
         
@@ -41,61 +44,47 @@ exports.setApp = function(app, client)
             ln = 'User';
             try
             {
-                const token = require("./createJWT.js");
-                ret = token.createToken( fn, ln, id );
+                newToken = token.createToken( fn, ln, id );
             }
             catch(e)
             {
-                ret = {error:e.message};
+                error = e.message;
             }
         }
         else    
         {
-            ret = {error:"Login/Password incorrect"};
+            error = {error:"Login/Password incorrect"};
+            newToken = "";
         }
+        ret = { error:error, jwtToken:newToken };
         res.status(200).json(ret);
     });
-
-    app.post('/api/login', async (req, res, next) =>
-{
-    // incoming: login, password
-    // outgoing: id, firstName, lastName, error
-    var error = '';
-    const { login, password } = req.body;
-    const db = client.db('COP4331Cards');
-    const results = await
-    db.collection('Users').find({Login:login,Password:password}).toArray();
-    var id = -1;
-    var fn = '';
-    var ln = '';
-    var ret;
-    if( results.length > 0 )
-    {
-    id = results[0].UserId;
-    fn = results[0].FirstName;
-    ln = results[0].LastName;
-    try
-    {
-    const token = require("./createJWT.js");
-    ret = token.createToken( fn, ln, id );
-    }
-    catch(e)
-    {
-    ret = {error:e.message};
-    }
-    }
-    else
-    {
-    ret = {error:"Login/Password incorrect"};
-    }
-    res.status(200).json(ret);
-});
 
     app.post('/api/addcard', async (req, res, next) =>
     {
         // incoming: userId, color
-        // outgoing: error
-        const { userId, card } = req.body;
+        // outgoing: error, jwtToken
+        const { userId, card, jwtToken} = req.body;
+
+        // Validate JWT token
+        try
+        {
+            if(token.isExpired(jwtToken))
+            {
+                let r = {error: "JWT no longer valid", jwtToken:""};
+                res.status(200).json(r);
+                return;
+            }
+        }
+        catch(e)
+        {
+            console.log("JWT Error: " + e.message);
+            let r = {error: "Error validating JWT", jwtToken:""};
+            res.status(200).json(r);
+            return;
+        }
+
+        // Backend logic to add card
         const newCard = {Card:card,UserId:userId};
         var error = '';
         try
@@ -107,25 +96,72 @@ exports.setApp = function(app, client)
         {
             error = e.toString();
         }
-        var ret = { error: error };
+
+        // Refresh JWT token
+        let refreshedToken = null;
+        try
+        {
+            refreshedToken = token.refresh(jwtToken);
+        }
+        catch(e)
+        {
+            console.log("JWT Refresh Error: " + e.message);
+        }
+
+        let ret = { error: error, jwtToken: refreshedToken };
+
         res.status(200).json(ret);
     });
 
     app.post('/api/searchcards', async (req, res, next) =>
     {
         // incoming: userId, search
-        // outgoing: results[], error
+        // outgoing: results[], error, jwtToken
+
+        // Validate JWT token
         var error = '';
-        const { userId, search } = req.body;
+        const { userId, search, jwtToken } = req.body;
+        try
+        {
+            if(token.isExpired(jwtToken))
+            {
+                let r = {error: "JWT no longer valid", jwtToken:""}
+                res.status(200).json(r);
+                return;
+            }
+        }
+        catch(e)
+        {
+            console.log("JWT Error: " + e.message);
+            let r = {error: "Error validating JWT", jwtToken:""}
+            res.status(200).json(r);
+            return;
+        }
+
+        // Backend logic to search cards
+
         var _search = search.trim();
         const db = client.db('COP4331Cards');
-        const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'i'}}).toArray();
+        const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'i'}, "UserId": userId}).toArray(); //test if this actually works
         var _ret = [];
         for( var i=0; i<results.length; i++ )
         {
             _ret.push( results[i].Card );
         }
-        var ret = {results:_ret, error:error};
+
+        // Refresh JWT token
+        let refreshedToken = null;
+        try
+        {
+            refreshedToken = token.refresh(jwtToken);
+        }
+        catch(e)
+        {
+            console.log("JWT Refresh Error: " + e.message);
+        }
+
+        var ret = {results:_ret, error:error, jwtToken: refreshedToken};
+
         res.status(200).json(ret);
     });
 }
