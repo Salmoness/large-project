b55bdb0bb83d642fb0170f00c3a2b446f2cc445b
Aaron Maryniewski <aa526825@ucf.edu>
2025-07-13 21:28:30 -0400

    forgot .com in api domain oh silly me

--- frontend/src/components/APIBaseURL.tsx
+++ frontend/src/components/APIBaseURL.tsx
@@ -1,6 +1,6 @@
 export function getAPIBaseURL(): string {
   if (process.env.NODE_ENV != "development") {
-    return "http://hopethiswork:5000/api/";
+    return "http://hopethiswork.com:5000/api/";
   } else {
     return "http://localhost:5000/api/";
   }
