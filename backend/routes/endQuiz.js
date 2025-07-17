1991656e8bea853fa26296ab16a4f8affb502245
saymon <slrivas24@gmail.com>
2025-07-16 22:30:06 -0400

    endQuiz not needed, replaced by submitQuiz

--- backend/routes/endQuiz.js
+++
@@ -1,5 +0,0 @@
-// Gets called every time a user ends a quiz.
-// It will mark the current session as finished
-// It will check if all other sessions are finished
-// If all sessions are finished, it will mark the quiz game as finished
-
