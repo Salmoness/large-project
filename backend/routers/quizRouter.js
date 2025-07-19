6877c010541531da3750213931ee521a36a50ac4
saymon <slrivas24@gmail.com>
2025-07-18 22:21:03 -0400

    created route for leaderboard and history pages

--- backend/routers/quizRouter.js
+++ backend/routers/quizRouter.js
@@ -10,6 +10,7 @@ const { submitQuiz } = require("../routes/submitQuiz.js");
 const { getQuizLeaderboard } = require("../routes/getQuizLeaderboard.js");
 const { quizInfo } = require("../routes/quizInfo.js");
 const { quizHistory } = require("../routes/quizHistory.js");
+const { searchGame } = require("../routes/searchGame.js")
 
 quizRouter.post("/generate", generateQuiz);
 quizRouter.post("/generatePlayground", generatePlaygroundQuiz);
@@ -20,5 +21,6 @@ quizRouter.post("/submit", submitQuiz);
 quizRouter.post("/leaderboard", getQuizLeaderboard);
 quizRouter.post("/info", quizInfo);
 quizRouter.post("/history", quizHistory);
+quizRouter.post("/searchGame", searchGame);
 
 module.exports.quizRouter = quizRouter;
